1c604f7f44774dcd89704ce5ad7fcd0bd8aa76e8
Dylan Corvidae <dylancorvidae@gmail.com>
2019-07-10 19:48:16 -0700

    first app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+
+//Reference needed elements
+const result = document.getElementById('result');
+const nameInput = document.getElementById('name');
+
+//Action to take on user event
+function updateResult() {
+    console.log('function ran');
+}
